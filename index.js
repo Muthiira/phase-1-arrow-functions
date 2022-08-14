9f6295e39c1c158b70427cceaf01e44f014b3210
Muthiira <calebmuchiri48@gmail.com>
2022-08-14 13:33:09 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,16 @@
+function divide (num1, num2) {
+	num1 = 2000;
+	num2 = 100;
+	return num1/num2;
+}
+
+const square = function (num3) {
+	num3 = 2;
+	let x = 2;
+	return x * num3;
+}
+
+const add = function (a, b) {
+ return a + b;
+
+}
